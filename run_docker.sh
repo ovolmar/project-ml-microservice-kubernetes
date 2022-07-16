326d82d264538d56062baf01e9cb7f251054a84f
oz volmar <o.volmar@gmail.com>
2022-07-16 13:55:39 -0400

    Prediction#1 beers and bratz

--- run_docker.sh
+++ run_docker.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
-
-## Complete the following steps to get Docker running locally
-
+# Machine Learning Buld 
 # Step 1:
 # Build image and add a descriptive tag
-
+docker build -t ml-project-flask .
 # Step 2: 
 # List docker images
-
+docker image ls 
 # Step 3: 
 # Run flask app
+docker run -p 8000:80 ml-project-flask
